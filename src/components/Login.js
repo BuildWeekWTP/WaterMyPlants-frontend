faf2c0ae03aef633315f8a1eaaf9b74178facdfe
Garin Mccullick <glmccullick@gmail.com>
2021-05-25 20:00:20 -0500

    created and finished login state/authentication made axios call in plantlists got most of signup functionality done as well

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,39 @@
+import axios from 'axios';
 import React from 'react'
 
-function Login() {
+class Login extends React.Component {
+  state = {
+    Credentials: {
+      user_username:'ted',
+      user_password:'foobar',
+      user_email:'ted@place.com',
+      user_phone_number:'1875309'
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      Credentials: {
+        ...this.state.Credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+
+  login = e => {
+    e.preventDefault();
+    axios.post('https://waterplants5.herokuapp.com/api/auth/login', this.state.Credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      this.props.history.push('/myPlants')
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  render() {
+
   return(
     <>
       <div className="container">
@@ -8,18 +41,38 @@ function Login() {
           <h2>Login</h2>
         </div>
         
-        <form>
+        <form onSubmit={this.login}>
 
           <input
-            name="userName"
+            name="username"
             type="text"
+            value={this.state.Credentials.user_username}
+            onChange={this.handleChange}
             placeholder="Username"
           />
 
           <input
             name="password"
             type="password"
+            value={this.state.Credentials.user_password}
+            onChange={this.handleChange}
             placeholder="Password"
+          />
+
+          <input 
+          name="email"
+          type="email"
+          value={this.state.Credentials.user_email}
+          onChange={this.handleChange}
+          placeholder="Email"
+          />
+
+          <input 
+          name="phoneNumber"
+          type="tel"
+          value={this.state.Credentials.user_phone_number}
+          onChange={this.handleChange}
+          placeholder="Phone Number"
           />
 
           <button>Log In</button>
@@ -28,6 +81,7 @@ function Login() {
       </div>
     </>
   )
+  }
 }
 
 export default Login
