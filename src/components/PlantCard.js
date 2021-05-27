a4e7ab3ba32959af6b5733f159dd65b5c99f4dd7
Garin Mccullick <glmccullick@gmail.com>
2021-05-27 13:17:23 -0500

    added plant card as well as axios get request for it

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,45 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function PlantCard() {
+
+  const [list,setList] = useState([]);
+
+  const listMap = list.map(plant_id =>{
+    console.log(plant_id.plant_id,plant_id.plant_species, plant_id.plant_nickname, plant_id.plant_image_url)
+    return(
+      <div className="plant-card">
+        <div className="plant-image">
+          {plant_id.plant_image_url}
+        </div>
+  
+        <div className="plant-stats">
+          <h3 id="plant-name">{plant_id.plant_nickname}</h3>
+          <p>{plant_id.plant_species}</p>
+        </div>
+      </div>
+    )
+  })
+
+  useEffect(() =>{
+     axios.get('https://waterplants5.herokuapp.com/api/plants')/*, {      //work on getting list of specific signed in user made plants/ make new plant paramaters(id, name...)
+        headers: {
+          authorization: localStorage.getItem('token')
+        }
+      })*/
+      .then(res => {
+        console.log(res.data)
+        setList(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return(
   <>
-    <div className="plant-card">
-      <div className="plant-image">
-        {/* is background-image */}
-      </div>
-  
-    <div className="plant-stats">
-      <h3 id="plant-name">Mr.green</h3>
-      <p>Lorem lorem</p>
-    </div>
-  </div>
+    {listMap}
   </>
   )
 }
--- src/components/PlantForm.js
+++ src/components/PlantForm.js
@@ -1,7 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
-function PlantForm() {
+class PlantForm extends React.Component {
+
+
+  state = {
+      plant_id:2,
+      plant_nickname:'',
+      plant_species:'',
+      plant_h2ofrequency:'',
+      plant_image_url:null
+  };
+
+  handleChange = e => {
+    this.setState({
+        ...this.state,
+        [e.target.name]: e.target.value
+    });
+  }
+
+  plant = e => {
+    e.preventDefault();
+    axios.post('https://waterplants5.herokuapp.com/api/plants', this.state) 
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  render(){
   return(
     <>
       <div className="container">
@@ -9,18 +39,30 @@ function PlantForm() {
           <h2>Create New Plant</h2>
         </div>
 
-      <form>
+      <form onSubmit={this.plant}>
         
         <input
-          name="Name"
+          name="plant_nickname"
           type="text"
           placeholder="Nickname"
+          value={this.state.plant_nickname}
+          onChange={this.handleChange}
         />
         
         <input
-          name="Name"
+          name="plant_species"
           type="text"
           placeholder="Species"
+          value={this.state.plant_species}
+          onChange={this.handleChange}
+        />
+
+        <input
+          name="plant_h2ofrequency"
+          type="text"
+          placeholder="Water intake"
+          value={this.state.plant_h2ofrequency}
+          onChange={this.handleChange}
         />
 
         <button>Add</button>
@@ -31,6 +73,7 @@ function PlantForm() {
       </div>
     </>
   )
+  }
 }
 
 export default PlantForm
